7ab3c0d274b5c4e890869deacf5f64865279e127
jamesmc <james@mediacambridge.com>
2018-09-22 16:53:44 +0100

    Initial express boilerplate

---
+++ server/eosidenticons/app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+  if (req.method === 'OPTIONS') {
+		const headers = {};
+		headers["Access-Control-Allow-Origin"] = "*";
+		headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS";
+		headers["Access-Control-Allow-Credentials"] = false;
+		headers["Access-Control-Allow-Headers"] = "cache-control, Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization";
+		res.writeHead(200, headers);
+		res.end();
+  } else {
+  	res.header("Access-Control-Allow-Origin", "*");
+  	res.header("Access-Control-Allow-Headers", "cache-control, Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  }
+});
+
+app.use('/', indexRouter);
+app.use('/account', accountRouter);
+
+module.exports = app;
---
+++ server/eosidenticons/routes/account.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/:accountName', function(req, res, next) {
+  const accountName = String(req.params.apiKey).split(/[^a-zA-Z0-9]/).join('').toLowerCase(); // clean accountName string only lowercase and numbers
+
+  res.send(`<script>document.write('Account: ${accountName}');</script>`;
+});
+
+module.exports = router;
