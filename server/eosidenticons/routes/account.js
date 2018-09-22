a701fdc6a736d0c0300b20e633e4479a27352306
jamesmc <james@mediacambridge.com>
2018-09-22 17:00:49 +0100

    Testing on server

--- server/eosidenticons/routes/account.js
+++ server/eosidenticons/routes/account.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/:accountName', function(req, res, next) {
   const accountName = String(req.params.apiKey).split(/[^a-zA-Z0-9]/).join('').toLowerCase(); // clean accountName string only lowercase and numbers
 
-  res.send(`<script>document.write('Account: ${accountName}');</script>`;
+  res.send(`<script>document.write('Account: ${accountName}');</script>`);
 });
 
 module.exports = router;
