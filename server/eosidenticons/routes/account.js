fc5695d0ef0e5b5c6c66142e3f093514886e0749
jamesmc <james@mediacambridge.com>
2018-09-22 17:02:15 +0100

    Export account key in demo

--- server/eosidenticons/routes/account.js
+++ server/eosidenticons/routes/account.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/:accountName', function(req, res, next) {
-  const accountName = String(req.params.apiKey).split(/[^a-zA-Z0-9]/).join('').toLowerCase(); // clean accountName string only lowercase and numbers
+  const accountName = String(req.params.accountName).split(/[^a-zA-Z0-9]/).join('').toLowerCase(); // clean accountName string only lowercase and numbers
 
   res.send(`<script>document.write('Account: ${accountName}');</script>`);
 });
